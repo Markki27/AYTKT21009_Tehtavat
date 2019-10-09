bf5c3221c201231868b44f450cd750c779fa47a3
Markus Moisio <markus@boomsolutions.fi>
2019-10-09 14:41:58 +0300

    Teht 2.3

--- 2/2.3/src/components/Total.js
+++ 2/2.3/src/components/Total.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Total = ({ parts }) => {
-    let total = 0
-    parts.map(part => total += part.exercises)
+    //let total = 0
+    //parts.map(part => total += part.exercises)
+
+    const total =
+        parts.reduce( (s, p) => s + p.exercises,0)
 
     return (
         <p><b>Total of {total} exercises</b></p>
